2ed2e438b5a7faebb98393a0230ff7a3471e2530
pakxe <pigkill40@naver.com>
2025-01-24 17:27:03 +0900

    chore: 불필요한 파일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import {ThemeProvider} from '@emotion/react';
+import theme from './theme';
+import Router from './Router';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {ModalProvider} from './hooks/useModal';
+
 function App() {
-  return <div>hi</div>;
+  const queryClient = new QueryClient();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <ModalProvider>
+          <Router />
+        </ModalProvider>
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
