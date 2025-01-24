2cf599550ec9653aff95144958ddd94189649908
pakxe <pigkill40@naver.com>
2025-01-24 17:22:54 +0900

    feat: 영화 상세 정보 모달을 띄우기 위한 훅 구현

---
+++ src/hooks/useMovieDetailModal.tsx
@@ -0,0 +1,16 @@
+import MovieDetailModal from '../components/MovieDetailModal';
+import MODAL_KEYS from '../constants/modalKeys';
+import {useModalContext} from './useModal';
+
+const useMovieDetailModal = (id: number) => {
+  const {open} = useModalContext();
+
+  return {
+    open: () =>
+      open(MODAL_KEYS.MOVIE_DETAIL, (isOpen, onClose) => (
+        <MovieDetailModal isOpen={isOpen} onClose={onClose} id={id} />
+      )),
+  };
+};
+
+export default useMovieDetailModal;
