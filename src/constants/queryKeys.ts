0a0eff98f4bb8e342f52557884d2ad48c7e21bf6
pakxe <pigkill40@naver.com>
2025-01-24 17:22:36 +0900

    feat: tanstack의 쿼리키 선언

---
+++ src/constants/queryKeys.ts
@@ -0,0 +1,8 @@
+const QUERY_KEYS = {
+  MOVIE_LIST: 'movieList',
+  POPULAR_MOVIE_LIST: 'popularMovieList',
+  MOVIE_DETAIL: 'movieDetail',
+  MOVIE_STAR_RATING: 'movieStarRating',
+};
+
+export default QUERY_KEYS;
