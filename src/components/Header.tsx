a4953a0458f6540b142acd8cc3790a6c4cc890e8
pakxe <pigkill40@naver.com>
2025-02-05 00:32:35 +0900

    design: Header css추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,10 +11,27 @@ const Header = () => {
     <header
       css={css`
         width: 100%;
+        height: 72px;
+        background-color: #222;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
         padding: 16px;
+        border-bottom: 1px solid white;
+        margin-bottom: 48px;
       `}
     >
-      <h1 onClick={() => navigate(PATHS.HOME)}>
+      <h1
+        onClick={() => navigate(PATHS.HOME)}
+        css={css`
+          cursor: pointer;
+          user-select: none;
+          font-size: 2rem;
+          font-weight: bold;
+          letter-spacing: -0.1rem;
+          color: #f33f3f;
+        `}
+      >
         <img src={logo} alt="MovieList 로고" />
       </h1>
       <SearchBar />
