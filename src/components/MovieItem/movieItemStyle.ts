229e97a72dab363d356dcfeb3028ebd1ac9f6ab3
pakxe <pigkill40@naver.com>
2025-02-04 20:33:08 +0900

    refactor: MovieItem의 css분리

---
+++ src/components/MovieItem/movieItemStyle.ts
@@ -0,0 +1,38 @@
+import {css} from '@emotion/react';
+
+const container = css`
+  cursor: pointer;
+
+  max-width: 180px;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  gap: 4px;
+`;
+
+const posterImage = css`
+  border-radius: 8px;
+  width: 100%;
+
+  aspect-ratio: 2/3;
+  object-fit: cover;
+
+  overflow: hidden;
+`;
+
+const title = css`
+  overflow: hidden;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  display: -webkit-box;
+`;
+
+const movieItemStyle = {
+  container,
+  posterImage,
+  title,
+};
+
+export default movieItemStyle;
