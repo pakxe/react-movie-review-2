cb54517cd02b3504480013b2159e9ccffe45b9a3
pakxe <pigkill40@naver.com>
2025-02-04 20:33:36 +0900

    refactor: SkeletonMovieItem을 사용하도록 수정

--- src/components/MovieListSkeleton.tsx
+++ src/components/MovieListSkeleton.tsx
@@ -1,25 +1,16 @@
-import {css, useTheme} from '@emotion/react';
 import List from './List';
+import SkeletonMovieItem from './MovieItem/SkeletonMovieItem';
 
 const MovieListSkeleton = () => {
   // 지금 width를 가져와서 1024 이상이면 4개, 768 이상이면 3개, 768 미만 2개 배열을 만든다.
   const count = (window.innerWidth >= 1024 ? 4 : window.innerWidth >= 768 ? 3 : 2) * 3;
   const skeletons = Array.from({length: count}, (_, i) => i);
-  const theme = useTheme();
 
   return (
     <List>
       {skeletons.map(i => (
         <li key={i}>
-          <div
-            className="item-card"
-            css={css`
-              width: 180px;
-              height: 270px;
-
-              background-color: ${theme.colors.gray3};
-            `}
-          />
+          <SkeletonMovieItem />
         </li>
       ))}
     </List>
