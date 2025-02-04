9c6cdc77f4b26dc395bb360d8f1ce1f1e60b5dc2
pakxe <pigkill40@naver.com>
2025-02-04 20:34:42 +0900

    chore: 파일 이동

---
+++ src/components/MovieItem/MovieItem.tsx
@@ -0,0 +1,33 @@
+import {css} from '@emotion/react';
+import useMovieDetailModal from '../../hooks/useMovieDetailModal';
+import Text from '../Text';
+import movieItemStyle from './movieItemStyle';
+import Image from '../Image';
+
+type Props = Movie & {};
+
+const MovieItem = ({id, title, poster_path, vote_average}: Props) => {
+  const {open} = useMovieDetailModal(id);
+
+  return (
+    <div onClick={open} css={movieItemStyle.container}>
+      <Image
+        src={`${import.meta.env.VITE_IMAGE_BASE_URL}${poster_path}.jpg`}
+        loading="lazy"
+        alt={title}
+        cssProp={movieItemStyle.posterImage}
+        minWidth={180}
+        minHeight={270}
+      />
+      <Text type="bodyBold" cssProp={movieItemStyle.title}>
+        {title}
+      </Text>
+      <Text type="body">
+        <img src="assets/star_filled.png" alt="별점" height={24} width={24} />
+        {vote_average}
+      </Text>
+    </div>
+  );
+};
+
+export default MovieItem;
