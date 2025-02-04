d5c2efbac15ffc4625ffce0600a2b58fc8a3f15f
pakxe <pigkill40@naver.com>
2025-02-05 03:26:05 +0900

    feat: 모달의 높이가 image에 의존하므로 image높이를 가져와 scroll 렌더링 여부 결정

--- src/components/MovieDetailModal/MovieDetailModal.tsx
+++ src/components/MovieDetailModal/MovieDetailModal.tsx
@@ -8,6 +8,8 @@ import SkeletonMovieDetailModal from './SkeletonMovieDetailModal';
 import ImageWithSkeleton from '../ImageWithSkeleton';
 import style from './movieDetailModalStyle';
 import RetryFallback from '../RetryFallback';
+import {css} from '@emotion/react';
+import {SyntheticEvent, useRef, useState} from 'react';
 
 type Props = ModalProps & {
   id: number;
@@ -15,6 +17,16 @@ type Props = ModalProps & {
 
 const MovieDetailModal = ({isOpen, onClose, id}: Props) => {
   const {movieDetail, isFetching, refetch} = useGetMovieDetail(id);
+  const imgRef = useRef<HTMLImageElement | null>(null);
+  const [imageHeight, setImageHeight] = useState(0);
+
+  const checkImageWithHeight = (e: SyntheticEvent<HTMLImageElement>) => {
+    if (!(e.target instanceof HTMLImageElement)) return;
+
+    const img = e.target;
+
+    setImageHeight(img.clientHeight);
+  };
 
   if (isFetching) {
     return <SkeletonMovieDetailModal onClose={onClose} isOpen={isOpen} />;
@@ -42,12 +54,14 @@ const MovieDetailModal = ({isOpen, onClose, id}: Props) => {
 
         <div css={style.movieDetail}>
           <ImageWithSkeleton
+            ref={imgRef}
             src={`https://image.tmdb.org/t/p/w220_and_h330_face/${poster_path}.jpg`}
             loading="lazy"
             cssProp={style.poster}
+            onLoad={checkImageWithHeight}
           />
           <div css={style.movieInfoAndRating}>
-            <div css={style.movieInfoWrapper}>
+            <div css={[style.movieInfoWrapper]}>
               <div css={style.genreAndVote}>
                 <Text>{genres.map(({name}) => name).join(', ')}</Text>
                 <div css={style.voteAverage}>
@@ -55,7 +69,15 @@ const MovieDetailModal = ({isOpen, onClose, id}: Props) => {
                   <Text>{vote_average}</Text>
                 </div>
               </div>
-              <Text>{overview}</Text>
+              <Text
+                cssProp={css`
+                  height: ${imageHeight - imageHeight * 0.4}px;
+
+                  overflow-y: auto;
+                `}
+              >
+                {overview}
+              </Text>
             </div>
             <StarRating id={id} />
           </div>
