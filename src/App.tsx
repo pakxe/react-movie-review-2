10920eff71b0c54326b6ac76d9028a0a2cc28265
pakxe <pigkill40@naver.com>
2025-02-05 00:32:22 +0900

    design: css제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import {ThemeProvider} from '@emotion/react';
+import {Global, ThemeProvider} from '@emotion/react';
 import theme from './theme';
 import Router from './Router';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {ModalProvider} from './hooks/useModal';
+import {GlobalStyle} from './GlobalStyle';
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <ThemeProvider theme={theme}>
+      <Global styles={GlobalStyle} />
+
       <QueryClientProvider client={queryClient}>
         <ModalProvider>
           <Router />
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
-import './reset.css';
-import './common.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
