aa33e849981758243bfc2ecb06830af0bba62bae
pakxe <pigkill40@naver.com>
2025-02-04 20:23:58 +0900

    feat: 이미지 호출을 위한 url추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_API_ACCESS_TOKEN: string;
   readonly VITE_API_URL: string;
   readonly VITE_BASE_URL: string;
+  readonly VITE_IMAGE_BASE_URL: string;
 }
 
 interface ImportMeta {
