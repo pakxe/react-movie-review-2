1d9f935d8e886d310bf4dc2a44da5e7a13a9cfe9
pakxe <pigkill40@naver.com>
2025-02-04 21:26:05 +0900

    feat: path를 상수로 변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,14 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import PopularMovieListPage from './pages/PopularMovieListPage';
-import KeywordMovieListPage from './pages/KeywordMovieListPage';
+import SearchMovieListPage from './pages/SearchMovieListPage';
+import PATHS from './constants/paths';
 
 const Router = () => {
   return (
     <BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
       <Routes>
-        <Route path="/" element={<PopularMovieListPage />} />
-        <Route path="/:keyword" element={<KeywordMovieListPage />} />
+        <Route path={PATHS.HOME} element={<PopularMovieListPage />} />
+        <Route path={PATHS.SEARCH} element={<SearchMovieListPage />} />
       </Routes>
     </BrowserRouter>
   );
