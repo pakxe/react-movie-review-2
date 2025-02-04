354755534fd3b059ff63d4bc263983b32b738e19
pakxe <pigkill40@naver.com>
2025-02-05 00:32:45 +0900

    design: SearchBar css추가

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,6 +2,8 @@ import {useEffect, useRef, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import QUERY_PARAMETERS from '../constants/queryParameters';
 import PATHS from '../constants/paths';
+import {css} from '@emotion/react';
+import searchIcon from '../../public/assets/search_button.png';
 
 const SearchBar = () => {
   const [isInputVisible, setIsInputVisible] = useState(window.innerWidth >= 768);
@@ -35,9 +37,36 @@ const SearchBar = () => {
   };
 
   return (
-    <form className="search-box" onSubmit={handleSearch}>
-      {isInputVisible && <input type="text" placeholder="검색" ref={ref} name={QUERY_PARAMETERS.QUERY} />}
-      <button onClick={isInputVisible ? handleSearch : toggleInputVisible} className="search-button">
+    <form
+      css={css`
+        background: #fff;
+        padding: 8px;
+        border-radius: 4px;
+      `}
+      onSubmit={handleSearch}
+    >
+      {isInputVisible && (
+        <input
+          css={css`
+            border: 0;
+          `}
+          type="text"
+          placeholder="검색"
+          ref={ref}
+          name={QUERY_PARAMETERS.QUERY}
+        />
+      )}
+      <button
+        onClick={isInputVisible ? handleSearch : toggleInputVisible}
+        css={css`
+          width: 14;
+          border: 0;
+          text-indent: -1000;
+          color: transparent;
+          background: url('${searchIcon}') transparent no-repeat 0 1px;
+          background-size: contain;
+        `}
+      >
         검색
       </button>
     </form>
