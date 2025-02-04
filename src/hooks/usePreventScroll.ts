1399a4d022a283dbf6fc867152695024413836c9
pakxe <pigkill40@naver.com>
2025-02-05 03:36:03 +0900

    fix: 스크롤을 막을 때 스크롤이 있다면 그 width만큼을 유지하도록 해 레이아웃 변화 방지

--- src/hooks/usePreventScroll.ts
+++ src/hooks/usePreventScroll.ts
@@ -5,11 +5,19 @@ const usePreventScroll = (isOn: boolean) => {
     if (!isOn) return;
 
     const originalOverflow = document.body.style.overflow;
+    const originalPaddingRight = document.body.style.paddingRight;
+
+    // 스크롤바 너비 계산
+    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
 
     document.body.style.overflow = 'hidden';
+    if (scrollbarWidth > 0) {
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
+    }
 
     return () => {
       document.body.style.overflow = originalOverflow;
+      document.body.style.paddingRight = originalPaddingRight;
     };
   }, [isOn]);
 };
